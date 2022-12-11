13453e3512450358109563805ef7dfa350e9809c
Евгений Луцкий <akaDanila@yandex.ru>
2022-12-11 12:16:33 +0300

    v8 ревью. Поменял realeted_name group и реализовал сортировку во вью через него

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -20,7 +20,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:COUNT_POSTS]
+    posts = group.posts.all()[:COUNT_POSTS]
     context = {
         'group': group,
         'posts': posts,
